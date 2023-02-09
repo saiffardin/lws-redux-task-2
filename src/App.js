1da27e27ee89ae6bc4f8470f8e549bf3fa336d9d
Saif Chowdhury <saiffardin@gmail.com>
2023-02-09 19:47:40 +0600

    redux provider added in app.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
+import {Provider} from 'react-redux';
 import './App.css';
 import Cart from './Components/Cart/Cart';
 import ProductList from './Components/ProductList/ProductList';
+import store from './redux/store';
 
 function App() {
 
     return (
-        <div className="bg-gray-50 h-full md:h-screen">
-            {/* title */}
-            <div className="grid place-items-center">
-                <h1
-                    className="text-gray-900 font-bold text-3xl p-10 underline decoration-purple-500 decoration-4 underline-offset-8 mb-4"
-                >
-                    Shopping Cart
-                </h1>
+        <Provider store={store}>
+            <div className="bg-gray-50 h-full md:h-screen">
+                {/* title */}
+                <div className="grid place-items-center">
+                    <h1
+                        className="text-gray-900 font-bold text-3xl p-10 underline decoration-purple-500 decoration-4 underline-offset-8 mb-4"
+                    >
+                        Shopping Cart
+                    </h1>
+                </div>
+
+                {/* main section */}
+                <div className="grid grid-cols-12 gap-6">
+
+                    {/* left section */}
+                    <ProductList />
+
+                    {/* right section */}
+                    <Cart />
+                </div>
             </div>
-
-            {/* main section */}
-            <div className="grid grid-cols-12 gap-6">
-
-                {/* left section */}
-                <ProductList />
-
-                {/* right section */}
-                <Cart />
-            </div>
-        </div>
+        </Provider>
     );
 }
 
